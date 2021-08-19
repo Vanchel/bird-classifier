53e7e47d8f06b4e36364177d84d87be4ac40f566
Vanchel <vanchel0305112551@gmail.com>
2021-08-19 23:39:14 +0300

    Add support for selecting an image from the gallery.

--- app/src/main/java/com/vanchel/birdclassifier/fragments/CameraFragment.kt
+++ app/src/main/java/com/vanchel/birdclassifier/fragments/CameraFragment.kt
@@ -1,24 +1,18 @@
 package com.vanchel.birdclassifier.fragments
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.GetContent
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
@@ -26,12 +20,10 @@ import com.vanchel.birdclassifier.R
 import com.vanchel.birdclassifier.databinding.FragmentCameraBinding
 import com.vanchel.birdclassifier.viewmodels.CameraViewModel
 import java.io.File
-import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-private const val TAG = "CameraFragment"
 private const val FILENAME = "buff.jpg"
 
 class CameraFragment : Fragment() {
@@ -43,9 +35,7 @@ class CameraFragment : Fragment() {
     private lateinit var cameraExecutor: ExecutorService
 
     private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
+        registerForActivityResult(RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
                 viewModel.onPermissionGranted()
             } else {
@@ -53,8 +43,18 @@ class CameraFragment : Fragment() {
             }
         }
 
+    private val getContentLauncher =
+        registerForActivityResult(GetContent()) { uri: Uri? ->
+            uri?.let {
+                findNavController().navigate(
+                    CameraFragmentDirections.actionCameraFragmentToResultFragment(it)
+                )
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
@@ -97,6 +97,20 @@ class CameraFragment : Fragment() {
         cameraExecutor.shutdown()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_camera_fragment, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.chooseFromGallery -> {
+                getContentLauncher.launch("image/*")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun requestCameraPermission() {
         when {
             ContextCompat.checkSelfPermission(
@@ -118,7 +132,6 @@ class CameraFragment : Fragment() {
     }
 
     private fun takePhoto() {
-        Log.i(TAG, if (imageCapture != null) "ok" else "not good")
         val imageCapture = imageCapture ?: return
 
         val photoFile = File(outputDirectory, FILENAME)
@@ -129,9 +142,7 @@ class CameraFragment : Fragment() {
             outputOptions,
             ContextCompat.getMainExecutor(requireContext()),
             object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
+                override fun onError(exc: ImageCaptureException) {}
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     findNavController().navigate(
@@ -139,7 +150,6 @@ class CameraFragment : Fragment() {
                             Uri.fromFile(photoFile)
                         )
                     )
-                    Log.i(TAG, "Photo capture succeeded: ${output.savedUri}")
                 }
             })
     }
@@ -165,7 +175,6 @@ class CameraFragment : Fragment() {
                     this, cameraSelector, preview, imageCapture
                 )
             } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
             }
 
         }, ContextCompat.getMainExecutor(requireContext()))
@@ -173,7 +182,6 @@ class CameraFragment : Fragment() {
 
     private fun getOutputDirectory(): File {
         val mediaDir = requireActivity().externalMediaDirs.firstOrNull()?.let {
-            Log.i(TAG, "we have our folder")
             File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
         }
         return if (mediaDir != null && mediaDir.exists())
